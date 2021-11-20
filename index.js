6c95c7939c20748cda98c5d05af9c65abc04f860
Titus Rainey <titusrainey@gmail.com>
2021-11-20 17:46:17 -0500

    created files and connected

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //Card values for the game
-let card1 = 6;
+let card1 = 10;
 let card2 = 11;
+let cards = [card1, card2];
 let cardSum = card1 + card2;
 // boolean for if the user gets blackjack
 let gotBlackJack = false;
@@ -9,9 +10,20 @@ let userIsAlive = true;
 let message = " ";
 // grabbing message-el from html and storing the info into js
 let messageEl = document.getElementById("message-el");
-console.log(messageEl)
+
+let sumEl = document.getElementById("sum-el");
+
+let cardsEl = document.getElementById("cards-el");
 
 let startGame = () => {
+    renderGame();
+}
+
+let renderGame = () => {
+
+sumEl.textContent = "Sum: " + cardSum;
+cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1];
+
   if (cardSum <= 20) {
     message = "Hit?";
   } else if (cardSum === 21) {
@@ -23,7 +35,13 @@ let startGame = () => {
     message = "Busted";
     userIsAlive = false;
   }
-messageEl.textContent = message  
+
+messageEl.textContent = message;
 }
 
-
+let newCard = () => {
+    console.log("Drawing a new card!");
+    let card = 2;
+    cardSum += card;
+    renderGame();
+}
