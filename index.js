d503edc9c15f190270b716ba8b64786a6d8227d6
Titus Rainey <titusrainey@gmail.com>
2021-11-20 12:25:50 -0500

    created files and connected

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
+//Card values for the game
 let card1 = 10;
 let card2 = 11;
 let cardSum = card1 + card2;
 
-if (cardSum < 21) {
-    console.log("Hit?")
+// boolean for if the user gets blackjack
+let gotBlackJack = false;
+// boolean for if the user's hand goes over 21
+let userIsAlive = true;
+
+if (cardSum <= 20) {
+    console.log("Hit?");
 } else if (cardSum === 21) {
-    console.log("Blackjack 21!!!")
+    console.log("Blackjack 21!!!");
+    /* the boolean gets turned to true if 
+    the user gets blackjack*/
+    gotBlackJack = true;
 } else {
-    console.log("Busted")
+    console.log("Busted");
+    userIsAlive = false;
 }
+
+//checking the booleans
+console.log(gotBlackJack);
+console.log(userIsAlive);
